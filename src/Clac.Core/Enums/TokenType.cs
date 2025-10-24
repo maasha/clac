8e0096bdcd99712991f01fb27244662828c3aef8
Martin Asser Hansen <maha@pharmait.dk>
2025-10-24 19:40:55 +0200

    Add TokenType enum to represent number and operator tokens in the calculator

---
+++ src/Clac.Core/Enums/TokenType.cs
@@ -0,0 +1,15 @@
+/// <summary>
+/// Enum for the types of tokens in the calculator which are either numbers or
+/// operator symbols.
+/// </summary>
+public enum TokenType
+{
+    /// <summary>
+    /// A number token.
+    /// </summary>
+    Number,
+    /// <summary>
+    /// An operator token.
+    /// </summary>
+    Operator
+}
