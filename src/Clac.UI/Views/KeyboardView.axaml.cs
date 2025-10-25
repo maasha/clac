4d0036df021502fd86fbe3b563fd9799a2206f29
Martin Asser Hansen <maha@pharmait.dk>
2025-10-25 19:38:06 +0200

    Add KeyboardView component to Calculator UI; integrate it into CalculatorView layout

---
+++ src/Clac.UI/Views/KeyboardView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Clac.UI.Views;
+
+public partial class KeyboardView : UserControl
+{
+    public KeyboardView()
+    {
+        InitializeComponent();
+    }
+}
