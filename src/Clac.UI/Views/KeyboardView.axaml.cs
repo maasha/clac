3933ac6221d28336da5f0194b360061d7639227e
Martin Asser Hansen <maha@pharmait.dk>
2025-11-06 12:25:30 +0100

    Update Undo key label in KeyboardView to improve clarity and user experience.

--- src/Clac.UI/Views/KeyboardView.axaml.cs
+++ src/Clac.UI/Views/KeyboardView.axaml.cs
@@ -36,7 +36,7 @@ public partial class KeyboardView : UserControl
         Key5View.DataContext = new KeyboardKey { Label = "5", Value = "5", Type = KeyType.Number };
         Key6View.DataContext = new KeyboardKey { Label = "6", Value = "6", Type = KeyType.Number };
         MinusKeyView.DataContext = new KeyboardKey { Label = "-", Value = "-", Type = KeyType.Operator };
-        UndoKeyView.DataContext = new KeyboardKey { Label = "D", Value = "", Type = KeyType.Command };
+        UndoKeyView.DataContext = new KeyboardKey { Label = "UNDO", Value = "", Type = KeyType.Command };
 
         // Row 4: 1, 2, 3, +, POP
         Key1View.DataContext = new KeyboardKey { Label = "1", Value = "1", Type = KeyType.Number };
