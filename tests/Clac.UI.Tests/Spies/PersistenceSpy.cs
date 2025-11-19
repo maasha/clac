1b13a15b3b3be221790cd9f0edb56e9159f163a3
Martin Asser Hansen <maha@pharmait.dk>
2025-11-19 21:05:56 +0100

    Updating persistenceSpy to return given result

--- tests/Clac.UI.Tests/Spies/PersistenceSpy.cs
+++ tests/Clac.UI.Tests/Spies/PersistenceSpy.cs
@@ -2,12 +2,15 @@ using Clac.Core.Services;
 using DotNext;
 using Clac.Core.History;
 using System.IO.Abstractions;
+using Clac.Core.Rpn;
 
 namespace Clac.UI.Tests.Spies;
 
 public class PersistenceSpy : IPersistence
 {
     public int SaveCallCount { get; private set; }
+    public int LoadCallCount { get; private set; }
+    public Result<StackAndInputHistory?> LoadResultToReturn { get; set; }
 
     public PersistenceSpy(IFileSystem fileSystem)
     {
@@ -20,7 +23,12 @@ public class PersistenceSpy : IPersistence
         return new Result<bool>(true);
     }
 
-    public Result<StackAndInputHistory?> Load(string? filePath = null) => new((StackAndInputHistory?)null);
+    public Result<StackAndInputHistory?> Load(string? filePath = null)
+    {
+        LoadCallCount++;
+        return LoadResultToReturn;
+    }
+
     public bool HasError => false;
     public string GetError() => "";
     public void ClearError() { }
