5976f46b39c53090a1be63a02e31a47c03820794
Martin Asser Hansen <maha@pharmait.dk>
2025-11-25 15:51:45 +0100

    Refactor PopCommandTests to use a consistent variable name for command instances, improving code clarity and maintainability.

--- tests/Clac.Core.Tests/Commands/PopCommandTests.cs
+++ tests/Clac.Core.Tests/Commands/PopCommandTests.cs
@@ -1,6 +1,5 @@
 using Clac.Core.Commands;
 using Clac.Core.Rpn;
-using static Clac.Core.ErrorMessages;
 
 namespace Clac.Core.Tests.Commands;
 
@@ -9,23 +8,23 @@ public class PopCommandTests
     [Fact]
     public void Name_ShouldBeCorrect()
     {
-        var popCommand = new PopCommand();
-        Assert.Equal("Pop", popCommand.Name);
+        var command = new PopCommand();
+        Assert.Equal("Pop", command.Name);
     }
 
     [Fact]
     public void Description_ShouldBeCorrect()
     {
-        var popCommand = new PopCommand();
-        Assert.Equal("Removes the last number from the stack", popCommand.Description);
+        var command = new PopCommand();
+        Assert.Equal("Removes the last number from the stack", command.Description);
     }
 
     [Fact]
     public void Execute_WithEmptyStack_ShouldDoNothing()
     {
-        var popCommand = new PopCommand();
+        var command = new PopCommand();
         var stack = new Stack();
-        var result = popCommand.Execute(stack);
+        var result = command.Execute(stack);
         Assert.True(result.IsSuccessful);
         Assert.Equal(0, result.Value);
     }
@@ -33,12 +32,12 @@ public class PopCommandTests
     [Fact]
     public void Execute_WithNonEmptyStack_ShouldPopTheLastNumber()
     {
-        var popCommand = new PopCommand();
+        var command = new PopCommand();
         var stack = new Stack();
         stack.Push(1);
         stack.Push(2);
         Assert.Equal(2, stack.Count);
-        var result = popCommand.Execute(stack);
+        var result = command.Execute(stack);
         Assert.True(result.IsSuccessful);
         Assert.Equal(2, result.Value);
         Assert.Equal(1, stack.Count);
